eaf5aa6e87e0bcd58f3a398cc9e1d02750c7ee50
doucx <doucxldh@gmail.com>
2026-01-08 17:09:08 +0800

    chore: remove useless data and move doc

--- packages/cascade-sdk/src/cascade/providers/__init__.py
+++
@@ -1,3 +0,0 @@
-# This allows 'cascade.providers' to be a namespace package,
-# extended by other installed packages (like cascade-provider-ipfs).
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)

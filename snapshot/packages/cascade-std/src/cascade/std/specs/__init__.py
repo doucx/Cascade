fc51b1e7334b1e31a4214869392bd7575186a052
doucx <doucxldh@gmail.com>
2026-01-09 16:18:51 +0800

    Capture: M __init__.py
    X-Quipu-Output-Tree: 656f7b4010370a03ad4d364dd0612cf3d6a8552a

--- snapshot/packages/cascade-std/src/cascade/std/specs/__init__.py
+++ snapshot/packages/cascade-std/src/cascade/std/specs/__init__.py
@@ -1,11 +1,12 @@
 from .resource import DiscreteAllocatorSpec, DiscreteReclaimerSpec
-from .triad import BleacherSpec, WorkerSpec, StainerSpec
+from .triad import BleacherSpec, WorkerSpec, StainerSpec, ObservabilitySpec
 from .system import EgressSpec, GateSpec, SleepSpec
 
 __all__ = [
     "DiscreteAllocatorSpec",
     "DiscreteReclaimerSpec",
     "BleacherSpec",
+    "ObservabilitySpec",
     "WorkerSpec",
     "StainerSpec",
     "EgressSpec",
